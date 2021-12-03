27530f8c5af6742ce5a1b69ad5bd53319b35f2c9
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-12-03 08:53:35 -0500

    Add ability to switch between themes

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 const greeting = document.getElementById('greeting')
 const btn = document.getElementById('btn')
 
-btn.addEventListener('click', fix)
+let isScaryTheme = true
 
-function fix() {
-  greeting.style.fontFamily = 'Mountains of Christmas'
-  greeting.textContent = '🎅 Merry Christmas! 🎄'
-  document.body.style.backgroundImage = `url('./images/christmas.jpg')`
+const scaryTheme = {
+  fontFamily: 'Rubik Beastly',
+  message: '🎃 Scary Christmas! 👻',
+  imageURL: `url('./images/scary-skeletons.jpg')`,
+  btnText: 'unscare me!',
 }
+const xmasTheme = {
+  fontFamily: 'Mountains of Christmas',
+  message: '🎅 Merry Christmas! 🎄',
+  imageURL: `url('./images/christmas.jpg')`,
+  btnText: 'scare me!',
+}
+
+btn.addEventListener('click', changeTheme)
+
+function changeTheme() {
+  greeting.style.fontFamily = isScaryTheme
+    ? xmasTheme.fontFamily
+    : scaryTheme.fontFamily
+
+  greeting.textContent = isScaryTheme ? xmasTheme.message : scaryTheme.message
+  btn.textContent = isScaryTheme ? xmasTheme.btnText : scaryTheme.btnText
+  document.body.style.backgroundImage = isScaryTheme
+    ? xmasTheme.imageURL
+    : scaryTheme.imageURL
+
+  isScaryTheme = !isScaryTheme
+}
+
+// function changeTheme() {
+//   greeting.style.fontFamily = 'Mountains of Christmas'
+//   greeting.textContent = '🎅 Merry Christmas! 🎄'
+//   document.body.style.backgroundImage = `url('./images/christmas.jpg')`
+// }
 
 // Task:
 // - Write a function to fix the UI problems with this Christmas message (make it Christmassy!)
